37070c812fc89f0bcf7c93e59eaec3fafec8fa0e
Peng Hou <hope-11@163.com>
2016-05-27 16:54:46 +0800

    req, res, handlebars, form

--- meadowlark.js
+++ meadowlark.js
@@ -2,23 +2,105 @@ var express=require('express');
 
 var app=express();
 
-var handlebars=require('express3-handlebars').create({defaultLayout:'main'});
+var handlebars=require('express3-handlebars').create({
+	defaultLayout:'main',
+	helpers: {
+		section: function(name, options){
+			if(!this._sections) this._sections={};
+			this._sections[name]=options.fn(this);
+			return null;
+		}
+	}
+});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 var fortune=require("./lib/fortune.js");
 
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
+function getWeatherData(){
+	return {
+		locations: [
+			{
+				name: 'portland',
+				forecastUrl:'http://www.wunderground.com/US/OR/Portland.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+				weather: 'Overcast',
+				temp: '54.1 F (12.3 C)'
+			},
+			{
+				name: 'Bend',
+				forecastUrl:'http://www.wunderground.com/US/OR/Bend.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+				weather: 'Partly Cloudy',
+				temp: '55.0 F (12.8 C)'
+			},
+			{
+				name: 'Manzanita',
+				forecastUrl:'http://www.wunderground.com/US/OR/Manzanita.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+				weather: 'LightRain',
+				temp: '55.0 F (12.8 C)'
+			}
+		]
+	};
+};
 
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname+'/public'));
 
+app.use(function(req, res, next){
+	res.locals.showTests=app.get('env') !== 'production' && req.query.test === '1';
+	next();
+});
+
+app.use(require('body-parser')());
+
+app.use(function(req, res, next){
+	if(!res.locals.partials) res.locals.partials={};
+	res.locals.partials.weather=getWeatherData();
+	next();
+});
+
 app.get('/', function(req, res){
 	res.render('home');
 });
 app.get('/about', function(req, res){
-	res.render('about', {fortune: fortune.getFortune()});
+	res.render('about', {
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js'
+	});
 });
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
+});
+app.get('/jquerytest', function(req, res){
+	res.render('jquerytest');
+});
+app.get('/nursery-rhyme', function(req, res){
+	res.render('nursery-rhyme');
+});
+app.get('/data/nursery-rhyme', function(req, res){
+	res.json({
+		animal:'squirrel',
+		bodyPart: 'tail',
+		adjective: 'bushy',
+		noun: 'heck'
+	});
+});
+app.get('/newsletter', function(req, res){
+	res.render('newsletter', {csrf: 'CSRF token goes here'});
+});
+app.post('/process', function(req, res){
+	console.log('Form (from queyrstring): ' + req.query.form);
+	console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+	console.log('Name (from visible form field): ' + req.body.name);
+	console.log('Email (from visible form field): ' + req.body.email);
+	res.redirect(303, '/thank-you');
+})
 
 //定制404页面
 app.use(function(req, res, next){
@@ -30,7 +112,7 @@ app.use(function(req, res, next){
 app.use(function(err, req, res, next){
 	console.error(err.stack);
 	res.status(500);
-	res.render('500')
+	res.render('500');
 });
 
 app.listen(app.get('port'), function(){
